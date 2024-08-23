6915ed46bd2d3575521d849e70b9be80ae388bec
Minkyu Jeong <cubone0417@gmail.com>
2024-08-23 15:30:08 +0900

    Add Medium Lv SQL coding test query
    LeetCode 코딩테스트 Medium Lv 문제

---
+++ LeetCode/Medium/177. Nth Highest Salary.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT IFNULL(
+            (
+                SELECT DISTINCT salary
+                FROM Employee
+                ORDER BY salary DESC
+                LIMIT 1 OFFSET N
+            ),
+            NULL
+        )
+    );
+END;
