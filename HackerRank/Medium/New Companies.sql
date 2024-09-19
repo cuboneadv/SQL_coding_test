0d2dbb9fd0613d6873f4fb7cc798e6564e4d5afe
Minkyu Jeong <cubone0417@gmail.com>
2024-09-19 18:18:04 +0900

    Add Medium Lv SQL coding test query
    HackerRank 코딩테스트 Medium Lv 문제

---
+++ HackerRank/Medium/New Companies.sql
@@ -0,0 +1,17 @@
+SELECT 
+    c.company_code, 
+    c.founder,
+    COUNT(DISTINCT lm.lead_manager_code) AS lead_manager_count,
+    COUNT(DISTINCT sm.senior_manager_code) AS senior_manager_count,
+    COUNT(DISTINCT m.manager_code) AS manager_count,
+    COUNT(DISTINCT e.employee_code) AS employee_count
+FROM 
+    Company c
+    INNER JOIN Lead_Manager lm ON c.company_code = lm.company_code
+    INNER JOIN Senior_Manager sm ON c.company_code = sm.company_code
+    INNER JOIN Manager m ON c.company_code = m.company_code
+    INNER JOIN Employee e ON c.company_code = e.company_code
+GROUP BY 
+    c.company_code, c.founder
+ORDER BY 
+    c.company_code ASC;
